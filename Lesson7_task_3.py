968afac34c7c0d19d1e1046302f3d05fcf7fa6dd
Aleksey <max_89@inbox.ru>
2021-03-18 14:57:50 +0300

    lesson_8

--- Lesson7_task_3.py
+++
@@ -1,9 +0,0 @@
-import os
-from shutil import copytree
-
-root_dir = 'my_project'
-dir_name = 'tamplates'
-
-for root, dirs, files in os.walk(root_dir):
-    if root.find(dir_name) > 0 and len(files) == 0:
-        copytree(os.path.join(root), dir_name, dirs_exist_ok=True)
